ee0892ddd898d898dee18ada67a7cd42717dcc0f
carl0sR0ma0 <cvpromao@gmail.com>
2021-04-06 09:49:23 -0300

    Add project files.

---
+++ DevCars.API/Persistence/DevCarsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DevCars.API.Entities;
+using DevCars.API.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DevCars.API.Persistence
+{
+    public class DevCarsDbContext : DbContext
+    {
+        public DevCarsDbContext(DbContextOptions<DevCarsDbContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ExtraOrderItem> ExtraOrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //modelBuilder.ApplyConfiguration(new CarDbConfiguration());
+            //modelBuilder.ApplyConfiguration(new CustomerDbConfiguration());
+            //modelBuilder.ApplyConfiguration(new OrderDbConfiguration());
+            //modelBuilder.ApplyConfiguration(new ExtraOrderItemDbConfiguration());
+        }
+    }
+}
